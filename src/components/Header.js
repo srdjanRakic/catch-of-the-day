e97a66275a352537fa4e25bc20ba0701bfea0592
srdjanRakic <srg_ian@hotmail.com>
2017-11-20 16:08:16 +0100

    Implement Header view.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,17 @@ class Header extends React.Component {
     render() {
         return (
             <header className="top">
-                <h1>Catch of the Day</h1>
-                <h3 className="tagline">Fill me in</h3>
+                <h1>
+                    Catch
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                    Day
+                </h1>
+                <h3 className="tagline">
+                    <span>{this.props.tagline}</span>
+                </h3>
             </header>
         );
     }
