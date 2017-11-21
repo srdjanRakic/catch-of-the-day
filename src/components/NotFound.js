c7e31fc46f0fc1bf2580d6986d7160266605baef
srdjanRakic <srg_ian@hotmail.com>
2017-11-21 10:36:01 +0100

    Add NotFound component.

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+    state = {  }
+    render() {
+        return (
+          <p>Page not found!</p>
+        );
+    }
+}
+
+export default NotFound;
