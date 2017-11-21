c32f0db0da332f0db678408c0054913b94dcdf75
srdjanRakic <srg_ian@hotmail.com>
2017-11-21 10:36:15 +0100

    Implement routing.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
-
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App />,
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} ></Match>
+                <Match exactly pattern="/store/:storeId" component={App} ></Match>
+                <Miss component={NotFound}></Miss>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+
+render(<Root />,
     document.getElementById('main')
 );
