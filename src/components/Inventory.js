d16e4ca48e62850333081846320e53a1af537324
srdjanRakic <srg_ian@hotmail.com>
2017-11-23 10:26:30 +0100

    Implement remove order from State.

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -54,6 +54,7 @@ class Inventory extends React.Component {
                        placeholder="Fish Image"
                        onChange={(e) => this.handleChange(e, key)}
                 />
+                <button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
             </div>
         )
     }
